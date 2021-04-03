42f1fed2e9d90be23ec19af78bcb9a0b0020b711
julieconnors <juliana.g.connors@gmail.com>
2021-04-03 06:20:51 -0400

    Done.

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews';
-
 class ReviewInput extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    debugger
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+    this.setState({
+      text: ""
+    })
+  }
+
   render() {
     return (
       <div>
         Review Input
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} value={this.state.text}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,37 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: []
+}, action) {
+    switch (action.type) {
+
+        case 'ADD_RESTAURANT':
+            const rez = {
+                text: action.text,
+                id: cuid()
+            }
+
+            return {...state, restaurants: [...state.restaurants, rez]};
+
+        case 'DELETE_RESTAURANT':
+            const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+            return { ...state, restaurants}
+
+        case 'ADD_REVIEW':
+            const rev = {
+                text: action.review.text,
+                restaurantId: action.review.restaurantId,
+                id: cuid()
+            }
+
+            return {...state, reviews: [...state.reviews, rev]};
+
+        case 'DELETE_REVIEW':
+            return {...state, reviews: state.reviews.filter(review => review.id !== action.id)}
+        
+        default:
+            return state;
+    }
 
 }
